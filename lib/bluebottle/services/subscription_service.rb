e4bb78dc7a8f38d703eeafa545c1ef9f1b883b72
Jordan Lawler <jordan.lawler@colorado.edu>
2016-05-22 16:31:27 -0600

    fixed issue with instance variable in subscription.rb

--- lib/bluebottle/services/subscription_service.rb
+++ lib/bluebottle/services/subscription_service.rb
@@ -12,14 +12,14 @@ module BlueBottle
       end
 
       def pause_subscription(customer, coffee)
-        subscription = @data_store.find_subscriptions_by_customer(customer, 'active').find do |subscription|
+        subscription = find_subscriptions_by_customer(customer, 'active').find do |subscription|
           subscription.coffee_name == coffee.name
         end
         subscription.pause
       end
 
       def cancel_subscription(customer, coffee)
-        subscription = @data_store.find_subscriptions_by_customer(customer).find do |subscription|
+        subscription = find_subscriptions_by_customer(customer).find do |subscription|
           subscription.coffee_name == coffee.name
         end
         cancel_switch(subscription)
@@ -37,12 +37,22 @@ module BlueBottle
         raise Exception.new('subscription paused')
       end
 
-      def find_subscriptions_by_customer(customer, status = 'any')
-        @data_store.find_subscriptions_by_customer(customer, status)
+      def find_subscriptions_by_coffee(coffee, status = 'any')
+        if status == 'active'
+          @data_store.find_active_subscriptions_by_coffee(coffee)
+        else
+          @data_store.find_all_subscriptions_by_coffee(coffee)
+        end
       end
 
-      def find_subscriptions_by_coffee(coffee, status = 'any')
-        @data_store.find_subscriptions_by_coffee(coffee, status)
+      def find_subscriptions_by_customer(customer, status = 'any')
+        if status == 'active'
+          @data_store.find_active_subscriptions_by_customer(customer)
+        elsif status == 'paused'
+          @data_store.find_paused_subscriptions_by_customer(customer)
+        else
+          @data_store.find_all_subscriptions_by_customer(customer)
+        end
       end
     end
   end
