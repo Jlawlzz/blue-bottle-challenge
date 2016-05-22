afc8866aecb3aa762cff5436ef4e817082928719
Jordan Lawler <jordan.lawler@colorado.edu>
2016-05-22 15:35:56 -0600

    possible alternative to current implementation, moves logic from subscription_service further into the data_store

--- lib/bluebottle/services/subscription_service.rb
+++ lib/bluebottle/services/subscription_service.rb
@@ -12,14 +12,14 @@ module BlueBottle
       end
 
       def pause_subscription(customer, coffee)
-        subscription = @data_store.find_active_subscriptions_by_customer(customer).find do |subscription|
+        subscription = @data_store.find_subscriptions_by_customer(customer, 'active').find do |subscription|
           subscription.coffee_name == coffee.name
         end
         subscription.pause
       end
 
       def cancel_subscription(customer, coffee)
-        subscription = @data_store.find_all_subscriptions_by_customer(customer).find do |subscription|
+        subscription = @data_store.find_subscriptions_by_customer(customer).find do |subscription|
           subscription.coffee_name == coffee.name
         end
         cancel_switch(subscription)
@@ -38,21 +38,11 @@ module BlueBottle
       end
 
       def find_subscriptions_by_customer(customer, status = 'any')
-        if status == 'active'
-          @data_store.find_active_subscriptions_by_customer(customer)
-        elsif status == 'paused'
-          @data_store.find_paused_subscriptions_by_customer(customer)
-        else
-          @data_store.find_any_subscriptions_by_customer(customer)
-        end
+        @data_store.find_subscriptions_by_customer(customer, status)
       end
 
       def find_subscriptions_by_coffee(coffee, status = 'any')
-        if status == 'active'
-          @data_store.find_active_subscriptions_by_coffee(coffee)
-        else
-          @data_store.find_all_subscriptions_by_coffee(coffee)
-        end
+        @data_store.find_subscriptions_by_coffee(coffee, status)
       end
     end
   end
